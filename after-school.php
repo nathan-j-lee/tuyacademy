fb92811affb77ffa9d9e1a6b1d397b4799df1626
Eric Chun <ericc@LDs-MacBook-Pro.local>
2023-07-05 16:29:53 -0700

    recommitting the entire codebase to github

---
+++ after-school.php
@@ -0,0 +1,59 @@
+<?php include 'templates/global_head.php'; ?>
+	<link rel="stylesheet" href="css/tuy_afterschool.css">
+</head>
+<?php include 'templates/global_header.php'; ?>
+    <h1>After School Daily Schedule</h1>
+    <div class="three_columns">
+        <div class="first_column">
+            <div class="h2_twocolumns">
+                <h2>Kindergarten</h2>
+                <div class="two_columns">
+                    <div class="column_left">
+                        <ul>
+                            <li>1:50-2:00 Pick Up</li>
+                            <li>2:15-3:00 Free Time</li>
+                            <li>3:00-3:30 Snacks</li>
+                            <li>3:30-4:45 Homework, Reading, Math, Writing</li>
+                            <li>4:45-5:15 Break Time</li>
+                            <li>5:15-6:00 Activities, Stories</li>
+                            <li>6:00-7:00 Free Time<</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="second_column">
+            <h2>1st - 5th Graders</h2>
+            <div class="column_left">
+                <ul>
+                    <li>3:00-3:30 Pick Up</li>
+                    <li>3:30-3:40 Snacks</li>
+                    <li>3:40-5:00 Homework, TUY Packet, Journaling</li>
+                    <li>5:00-5:15 Break Time</li>
+                    <li>5:15-6:00 Reading, Writing, Math</li>
+                    <li>6:00-7:00 Free Time</li>
+                </ul>
+            </div>
+        </div>
+        <div class="third_column">
+            <h2>6th - 8th Graders:</h2>
+            <div class="column_left">
+                <ul>
+                    <li>3:00-3:15</li>
+                    <li>3:15-7:00</li>
+                </ul>
+            </div>
+            <div class="column_right">
+                <ul>
+                    <li>Pick Up</li>
+                    <li>Snacks</li>
+                    <li>Homework</li>
+                    <li>Projects</li>
+                    <li>Report Helper</li>
+                    <li>Reading</li>
+                    <li>Writing</li>
+                    <li>Math: Pre-Algebra, Algebra, Geometry</li>
+                </ul>
+            </div>
+        </div>
+    </div>
