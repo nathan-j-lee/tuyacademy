050680bcd46aa291cdc619fddfaa67df4a3071f8
nathan-j-lee <127238376+nathan-j-lee@users.noreply.github.com>
2023-07-10 16:40:55 -0700

    Update times to match schedule and change uppercase to lowercase

--- after-school.php
+++ after-school.php
@@ -10,12 +10,12 @@
                 <div class="two_columns">
                     <div class="column_left">
                         <ul>
-                            <li>1:50-2:00 Pick Up</li>
-                            <li>2:15-3:00 Free Time</li>
-                            <li>3:30-4:45 Homework, Reading, Math, Writing</li>
-                            <li>4:45-5:15 Break Time</li>
-                            <li>5:15-6:00 Activities, Stories</li>
-                            <li>6:00-6:30 Free Time</li>
+                            <li>1:50pm-2:00pm Pick Up</li>
+                            <li>2:15pm-3:00pm Free Time</li>
+                            <li>3:30pm-4:45pm Homework, Reading, Math, Writing</li>
+                            <li>4:45pm-5:15pm Break Time</li>
+                            <li>5:15pm-6:00pm Activities, Stories</li>
+                            <li>6:00pm-6:30pm Free Time</li>
                         </ul>
                     </div>
                 </div>
@@ -25,12 +25,12 @@
             <h2>1st - 5th Graders</h2>
             <div class="column_left">
                 <ul>
-                    <li>3:00-3:30 Pick Up</li>
-                    <li>3:30-3:40 Snacks</li>
-                    <li>3:40-5:00 Homework, TUY Packet, Journaling</li>
-                    <li>5:00-5:15 Break Time</li>
-                    <li>5:15-6:00 Reading, Writing, Math</li>
-                    <li>6:00-6:30 Free Time</li>
+                    <li>3:00pm-3:30pm Pick Up</li>
+                    <li>3:30pm-3:40pm Snacks</li>
+                    <li>3:40pm-5:00pm Homework, TUY Packet, Journaling</li>
+                    <li>5:00pm-5:15pm Break Time</li>
+                    <li>5:15pm-6:00pm Reading, Writing, Math</li>
+                    <li>6:00pm-6:30pm Free Time</li>
                 </ul>
             </div>
         </div>
@@ -38,12 +38,12 @@
             <h2>6th - 8th Graders:</h2>
             <div class="column_right">
                 <ul>
-                    <li>3:00-3:30 Pick Up</li>
-                    <li>3:30-3:40 Snacks</li>
-                    <li>3:40-5:00 Homework, TUY Packet, Journaling</li>
-                    <li>5:00-5:15 Break Time</li>
-                    <li>5:15-6:00 Reading, Writing, Math</li>
-                    <li>6:00-6:30 Free Time</li>
+                    <li>3:00pm-3:30pm Pick Up</li>
+                    <li>3:30pm-3:40pm Snacks</li>
+                    <li>3:40pm-5:00pm Homework, TUY Packet, Journaling</li>
+                    <li>5:00pm-5:15pm Break Time</li>
+                    <li>5:15pm-6:00pm Reading, Writing, Math</li>
+                    <li>6:00pm-6:30pm Free Time</li>
                 </ul>
             </div>
         </div>
--- summer-school.php
+++ summer-school.php
@@ -11,7 +11,6 @@
                 <div class="fr_first_column">
                     <h2>Field Trips</h2>
                     <ul>
-                        <li>Beach</li>
                         <li>Seaside Lagoon</li>
                         <li>Ice Skating</li>
                         <li>Raging Waters</li>
@@ -41,13 +40,13 @@
                 <div class="daily_left">
                     <h2>Daily Schedule</h2>
                     <ul class="no_styles">
-                        <li>8:00AM TUY Open</li>
-                        <li>9:30AM - 10:15AM English</li>
-                        <li>10:15AM - 12:15AM Class</li>
-                        <li>12:15PM - 1:00PM Lunch (Provided)</li>
-                        <li>1:00PM - 2:00PM Reading/Journal Writing</li>
-                        <li>2:00PM - 4:00PM Activities/Playground</li>
-                        <li>4:30PM - 7:00PM Free Time and Closing</li>
+                        <li>8:00am Open</li>
+                        <li>9:30am - 12:15pm Classes (Reading, Writing, Math)</li>
+                        <li>12:15pm - 1:00pm Lunch (Provided)</li>
+                        <li>1:00pm - 2:00pm Reading/Journal Writing</li>
+                        <li>2:00pm - 4:00pm Sports/Activities/Playground</li>
+                        <li>4:00pm - 4:30pm Reading</li>
+                        <li>4:30pm - 6:30pm Free Time and Closing</li>
                     </ul>
                 </div>
             </div>
